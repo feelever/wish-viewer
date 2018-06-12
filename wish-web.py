cf200291de6fd854a046f2ccdac4d90b04a95e72
feelever <feelevercai@gmail.com>
2018-06-12 23:22:15 +0800

    修改业务逻辑

--- wish-web.py
+++ wish-web.py
@@ -3,31 +3,50 @@ from flask import render_template
 import time
 import wish
 import json
+import schedule
+import sys
+import _thread
 from apscheduler.schedulers.background import BackgroundScheduler
 app=Flask(__name__)
+label_yesterday=time.strftime("%Y%m%d",time.localtime(time.time()-24*60*60))+"_baught.json"
+label_today=str(time.strftime("%Y%m%d", time.localtime())) +"_baught.json"
 @app.route('/wish') 
-@app.route('/wish/<name>')
-def hello_world(name=None):
-    return render_template('index.html',name=name)
-@app.route('/wish/diff')
-def wish_diff():
-    label_yesterday=time.strftime("%Y%m%d",time.localtime(time.time()-24*60*60))+"_baught.json"
-    label_today=str(time.strftime("%Y%m%d", time.localtime())) +"_baught.json"
-    #diffs=[]
-    #crawlwish()
+@app.route('/wish/<keywords>')
+def hello_world(keywords=None):
+    if keywords==None:
+        keywords="phone+case"
+    start =time.strftime("%Y%m%d",time.localtime(time.time()-24*60*60))
+    end=str(time.strftime("%Y%m%d", time.localtime()))
+    return render_template('index.html',keywords=keywords,start=start,end=end)
+@app.route('/wish/diff/<keywords>/<start>/<end>')
+def wish_diff(keywords=None,start=None,end=None):
     result=[]
     try:
-        res = wish.cal_diff(label_yesterday,label_today)
-        for n in res:
-            #print(int(n["now"]))
-            if int(n["pre"])<100:
+        if len(start)<8:
+            start =time.strftime("%Y%m%d",time.localtime(time.time()-24*60*60))
+        if len(end)<8:
+            end=str(time.strftime("%Y%m%d", time.localtime()))
+        if keywords==None:
+            keywords="phone+case"
+        res = wish.cal_diff(keywords,start+".json",end+".json")
+        repeatId=[]
+        for n in res:            
+            if int(n["pre"])<100 and n["pre"]!=n["now"] and n['id'] not in repeatId:                
                 result.append(n)
+                repeatId.append(n["id"])
     except:
-        print("eror")
-    print(len(result))
+        print("Unexpected error:", sys.exc_info()[0])
     return json.dumps(result)
-if __name__ == '__main__': 
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(wish.crawlwish, 'cron', hour=17,minute=25,second=3)
-    scheduler.start()
+def job():
+    print("start job")
+    label_today=str(time.strftime("%Y%m%d", time.localtime()))
+    wish.crawlwish(label_today,"phone+case")
+    wish.crawlwish(label_today,"rings")
+def schejob():
+    schedule.every(10).seconds.do(job)
+    while True:
+       schedule.run_pending()
+       time.sleep(1)
+if __name__ == '__main__':
+    _thread.start_new_thread(schejob,())
     app.run(host='0.0.0.0',port="9561",debug=True)
--- wish.py
+++ wish.py
@@ -7,11 +7,9 @@ import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
 headers={}
-def get_num_id(num,start,cals,head):
-    #print(head)
-    r=requests.post("https://www.wish.com/api/search?start="+str(start)+"&query=phone+case&transform=true",headers=head)
+def get_num_id(num,start,cals,head,query):
+    r=requests.post("https://www.wish.com/api/search?start="+str(start)+"&query="+query+"&transform=true",headers=head)
     result = json.loads(r.text)
-    #print(result)
     for data in result["data"]["results"]:
         if "feed_tile_text" in data:
                 num=data["feed_tile_text"].replace(" bought this","")
@@ -26,12 +24,16 @@ def get_express(id):
         r=requests.post(url,headers)
         result = json.loads(r.text)
         return result
-def cal_diff(pre_date,now_date):
-        pre_date=os.path.join(APP_STATIC, pre_date)
-        now_date=os.path.join(APP_STATIC, now_date)
+def cal_diff(keywords,pre,now):
+        diffs=[]
+        pre_date=os.path.join(APP_STATIC+"\\"+keywords, pre)
+        #print(pre_date)
+        now_date=os.path.join(APP_STATIC+"\\"+keywords, now)
         pres=[]
         with open(pre_date,'r') as load_f:
                  pres = json.load(load_f)
+        #print(pres)
+        print(now_date)
         nows=[]
         with open(now_date,'r') as load_f:
                  nows = json.load(load_f)
@@ -46,7 +48,7 @@ def cal_diff(pre_date,now_date):
                                 diff["incr"]= incr
                                 diffs.append(diff)
         return diffs
-def crawlwish():
+def crawlwish(date,query):
         print("start")
         headers ={"Cookie":"bsid=50d9fabb01ec49faad2e96a63aadf7d6"}
         r=requests.get("https://www.wish.com",headers=headers)
@@ -61,19 +63,12 @@ def crawlwish():
         #product_ids =[]
         cals=[]
         start =0
-        while(start<200):
+        while(start<500):
                 print(start)
-                start+=get_num_id(10,start,cals,headers)
-        print(os.path.join(APP_STATIC,label_today))
-        with open(os.path.join(APP_STATIC, label_today),"w") as f:
+                start+=get_num_id(10,start,cals,headers,query)
+        with open(os.path.join(APP_STATIC+"/"+query, date+".json"),"w") as f:
                 json.dump(cals,f)
-label_yesterday=time.strftime("%Y%m%d",time.localtime(time.time()-24*60*60))+"_baught.json"
-label_today=str(time.strftime("%Y%m%d", time.localtime())) +"_baught.json"
-diffs=[]
-crawlwish()
-#cal_diff(label_yesterday,label_today)
-#print(diffs)
-
+#crawlwish("20180612","phone+case")
         
 
 
